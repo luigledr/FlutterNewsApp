80db92411c58931eed8056bd0f4c20f1be556685
Luiz Oliveira <luizoliveira@outlook.com>
2018-07-19 18:38:23 -0300

    Initial commit

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Top News"),
+      ),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            int itemId = snapshot.data[index];
+            bloc.fetchItem(snapshot.data[index]);
+
+            return NewsListTile(
+              itemId: itemId,
+            );
+          },
+        );
+      },
+    );
+  }
+}
